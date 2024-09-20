59b01ec0d9af9f4a624b6f51174e904b66f16c2a
Mehul0906 <mehulkurhe005@gmail.com>
2024-09-20 14:55:30 +0530

    update

---
+++ Redux Builder/src/Components/Counter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CounterValue from './CounterValue';
+import CounterButtons from './CounterButtons';
+
+
+const Counter = () => {
+  return (
+    <div className="counter-container" >
+      <CounterValue />
+      <CounterButtons />
+    </div>
+  );
+};
+
+export default Counter;
---
+++ Redux Builder/src/store/themeReducer.js
@@ -0,0 +1,36 @@
+// // themeReducer.js
+// const initialTheme = {
+//     theme: 'light',
+//   };
+  
+//   export const themeReducer = (state = initialTheme, action) => {
+//     switch (action.type) {
+//       case 'SET_THEME':
+//         return {
+//           ...state,
+//           theme: action.theme,
+//         };
+//       default:
+//         return state;
+//     }
+//   };
+  
+
+
+// themeReducer.js
+const initialTheme = {
+    theme: 'light',
+  };
+  
+  export const themeReducer = (state = initialTheme, action) => {
+    switch (action.type) {
+      case 'SET_THEME':
+        return {
+          ...state,
+          theme: action.theme,
+        };
+      default:
+        return state;
+    }
+  };
+  
